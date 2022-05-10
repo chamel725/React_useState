6022dce8f67e96fd3d6eda4d131656e9a5acfdd7
chamel <chanillee@Chanilui-MacBookAir.local>
2022-05-10 19:41:24 +0900

    123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState} from 'react';
+
 function App() {
+  //useState는 초기값으로 1이 담긴 time이라는 변수와
+  //time을 업데이트 하기 위한 setTime이라는 함수를 배열 형태로 변환
+  const [time, setTime] = useState(1);
+
+  //setTime함수로 state를 업데이트 시켜주면 comopnent는 browser상에서 다시 렌더링된다.
+  const handleClick = () =>{
+    setTime(time + 1)//setTime의 인자로는 setState로 반영될 값이 들어간다
+  };
+  console.log('업데이트');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>현재 시각: {time}시</span>
+      <button onClick={handleClick}>Update</button>
     </div>
   );
 }
